5d532f72eb46cf061a33d4752cafdecc85c7827b
Mick McDonnal <mickmcdonnal@gmail.com>
2019-01-15 17:09:13 -0800

    Improvements to django admin.

--- mysite/pics/admin.py
+++ mysite/pics/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from pics.models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'view_count', 'date_taken', 'wallpaper')
+    list_display = ('title', 'view_count', 'date_taken', 'wallpaper', 'show_photo_url')
+    search_fields = ('title',)
+    
+    def show_photo_url(self, obj):
+        return format_html("<a href='{url}' target='_blank'>{url}</a>", url=obj.source_url)
+
+    show_photo_url.short_description = "Photo on Flickr"
 
 admin.site.register(Photo, PhotoAdmin)
