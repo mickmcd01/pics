7f6eec262cccf5a1764c3d67afb5b25271b48017
Mick McDonnal <mickmcdonnal@gmail.com>
2019-02-10 14:46:51 -0800

    make statistics fields in admin read-only.

--- mysite/pics/admin.py
+++ mysite/pics/admin.py
@@ -98,6 +98,7 @@ admin.site.register(Photo, PhotoAdmin)
 
 class StatisticsAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'public_pics', 'view_count', 'edits')
+    readonly_fields = ['id', 'date', 'public_pics', 'view_count', 'edits']
 
     def has_add_permission(self, request, obj=None):
         return False
